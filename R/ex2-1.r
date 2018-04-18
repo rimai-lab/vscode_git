0e067742cffdc4a10c2d1dcbe01a114762157e6b
rimai <imai_ryo_lab@outlook.jp>
2018-04-18 16:19:51 +0900

    r_ex21

--- R/ex2-1.r
+++ R/ex2-1.r
@@ -6,7 +6,7 @@ par(mfcol = c(3, 4))
 xls <- list(c(3, 9), c(1, 6), c(0, 8), c(-1, 4))
 
 for (j in 1:4) {
-    for (i in 1:3){
+    for (i in 1:3) {
         hist(iris[iris[,5] == species[i], j], xlim = xls[[j]], ylim = c(0, 15), breaks = 15,
             xlab = colnames(iris)[1], main = species[i])
     }  
