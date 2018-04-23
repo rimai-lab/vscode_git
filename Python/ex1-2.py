d103df2fd1c75cf349df75730f0f593993163503
Imai Ryo <imai_ryo_lab@outlook.jp>
2018-04-23 12:18:27 +0900

    py_ex1_2

--- Python/ex1-2.py
+++ Python/ex1-2.py
@@ -1,13 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import csv
 
 x = []
 
 with open('output.csv', 'r') as f:
     r = csv.reader(f)
-    x = [i for i in r]
+    for i in r:
+        x += [[int(j) for j in i]]
+    
 
-l = []
-for j in x if j[0] % 2 == 0:
-    l += [j[0], j[2]]
+head = np.array(x)[:, 0]
+resx = np.array(x)[head % 2 == 0, 0]
+resy = np.array(x)[head % 2 == 0, 2]
 
-print(l)
+plt.scatter(resx, resy)
+plt.savefig("scatter.pdf")
